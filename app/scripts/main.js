87e89b67e5f6144720a4e7f3f0de05f28ee759c5
albertkai <albertkai@me.com>
2015-09-25 19:57:45 +0300

    mobile enhancements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,19 @@
 $(function(){
 
+    $(document).ready(function(){
+
+        $('body').addClass('_ready')
+
+    })
+
+    // Detect mobile with response.js library
+
+    var isMobile = Response.deviceW() < 767
+
+    // Setting nice ios-like scrolling for mobile
+
+    if (isMobile) $('html').niceScroll()
+
     // Setting locale (only russian and english versions now)
 
     var lang = window.navigator.userLanguage || window.navigator.language
@@ -39,6 +53,15 @@ $(function(){
 
     // Why and top
 
+    var topTween
+
+    if (!isMobile) {
+        topTween = new TimelineMax().add([
+            TweenMax.to('#top .iphone', 1, {'y': '300', ease: Power0.easeNone}),
+            TweenMax.to('#top article', 1, {'y': '300', scale: '.8', 'opacity': '0', ease: Power0.easeNone})
+        ])
+    }
+
     var circlesTween = new TimelineMax().add([
         TweenMax.from('.circles .graph .circle', 1, {'scale': '0.3', 'opacity': '0'}),
         TweenMax.from('.circles .graph p', 1, {'y': '200', 'opacity': '0', ease: Power0.easeNone})
@@ -47,11 +70,6 @@ $(function(){
     var topTextTween1 = TweenMax.from('#why .text1', 1, {'x': '120', 'opacity': '0'})
     var topTextTween2 = TweenMax.from('#why .text2', 1, {'x': '-120', 'opacity': '0'})
     var topHeaderTween = TweenMax.from('#why h2', 1, {scale: '1.5', y: '-70', opacity: 0})
-
-    var topTween = new TimelineMax().add([
-        TweenMax.to('#top .iphone', 1, {'y': '300', ease: Power0.easeNone}),
-        TweenMax.to('#top article', 1, {'y': '300', scale: '.8', 'opacity': '0', ease: Power0.easeNone})
-    ])
 
     var tableTween = new TimelineMax()
     tableTween.add(TweenMax.staggerFrom('.table .t-row', 2, {opacity:0, x:100, ease:Power0.easeNone},.7))
@@ -76,19 +94,31 @@ $(function(){
 
     var parallaxTween = TweenMax.from('#teaser > .pic', 1, {y: '-40%', ease:Power0.easeNone})
 
-    new ScrollMagic.Scene({duration: $(window).height(), triggerElement: '#top', offset: $(window).height() / 2}).setTween(topTween).addTo(controller)
-    new ScrollMagic.Scene({duration: 400, triggerElement: '#why'}).setTween(circlesTween).addTo(controller)
-    new ScrollMagic.Scene({duration: 500, triggerElement: '#why', offset: $(window).height() / 3 * -1}).setTween(topHeaderTween).addTo(controller)
-    new ScrollMagic.Scene({duration: 500, triggerElement: '#why', offset: 100}).setTween(topTextTween1).addTo(controller)
-    new ScrollMagic.Scene({duration: 700, triggerElement: '#why', offset: 400}).setTween(tableTween).addTo(controller)
-    new ScrollMagic.Scene({duration: 500, triggerElement: '#why', offset: 500}).setTween(topTextTween2).addTo(controller)
+    if (isMobile){
 
-    new ScrollMagic.Scene({duration: 500, triggerElement: '#how', offset: $(window).height() / 3 * -1}).setTween(howHeaderTween).addTo(controller)
-    new ScrollMagic.Scene({duration: 700, triggerElement: '#how'}).setTween(iPhoneTween).addTo(controller)
+        new ScrollMagic.Scene({duration: 200, triggerElement: '#why', offset: -150}).setTween(topHeaderTween).addTo(controller)
+        new ScrollMagic.Scene({duration: 250, triggerElement: '#why', offset: 150}).setTween(circlesTween).addTo(controller)
+        new ScrollMagic.Scene({duration: 500, triggerElement: '#why', offset: 650}).setTween(tableTween).addTo(controller)
+        new ScrollMagic.Scene({duration: 200, triggerElement: '#how', offset: -150}).setTween(howHeaderTween).addTo(controller)
+        new ScrollMagic.Scene({duration: 700, triggerElement: '#how'}).setTween(iPhoneTween).addTo(controller)
+        new ScrollMagic.Scene({duration: 200, triggerElement: '#safe', offset: -150}).setTween(safeHeaderTween).addTo(controller)
+        new ScrollMagic.Scene({duration: 350, triggerElement: '#safe', offset: 300}).setTween(shieldTween).addTo(controller)
+        new ScrollMagic.Scene({duration: $(window).height(), triggerElement: '#teaser', offset: $(window).height() / 2 * -1}).setTween(parallaxTween).addTo(controller)
 
-    new ScrollMagic.Scene({duration: 500, triggerElement: '#safe', offset: $(window).height() / 3 * -1}).setTween(safeHeaderTween).addTo(controller)
-    new ScrollMagic.Scene({duration: 500, triggerElement: '#safe'}).setTween(shieldTween).addTo(controller)
+    } else {
 
-    new ScrollMagic.Scene({duration: $(window).height(), triggerElement: '#teaser', offset: $(window).height() / 2 * -1}).setTween(parallaxTween).addIndicators().addTo(controller)
+        new ScrollMagic.Scene({duration: $(window).height(), triggerElement: '#top', offset: $(window).height() / 2}).setTween(topTween).addTo(controller)
+        new ScrollMagic.Scene({duration: 400, triggerElement: '#why'}).setTween(circlesTween).addTo(controller)
+        new ScrollMagic.Scene({duration: 500, triggerElement: '#why', offset: $(window).height() / 3 * -1}).setTween(topHeaderTween).addTo(controller)
+        new ScrollMagic.Scene({duration: 500, triggerElement: '#why', offset: 100}).setTween(topTextTween1).addTo(controller)
+        new ScrollMagic.Scene({duration: 700, triggerElement: '#why', offset: 400}).setTween(tableTween).addTo(controller)
+        new ScrollMagic.Scene({duration: 500, triggerElement: '#why', offset: 500}).setTween(topTextTween2).addTo(controller)
+        new ScrollMagic.Scene({duration: 500, triggerElement: '#how', offset: $(window).height() / 3 * -1}).setTween(howHeaderTween).addTo(controller)
+        new ScrollMagic.Scene({duration: 700, triggerElement: '#how'}).setTween(iPhoneTween).addTo(controller)
+        new ScrollMagic.Scene({duration: 500, triggerElement: '#safe', offset: $(window).height() / 3 * -1}).setTween(safeHeaderTween).addTo(controller)
+        new ScrollMagic.Scene({duration: 500, triggerElement: '#safe'}).setTween(shieldTween).addTo(controller)
+        new ScrollMagic.Scene({duration: $(window).height(), triggerElement: '#teaser', offset: $(window).height() / 2 * -1}).setTween(parallaxTween).addTo(controller)
+
+    }
 
 })
